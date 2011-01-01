9e7499297e247599ef049e71c1c9c3b893444741
windyakin <windyakin@gmail.com>
2011-01-01 12:39:45 +0000

    zerochplus trunk ・板トップのSamba値の表示を修正
    git-svn-id: http://svn.sourceforge.jp/svnroot/zerochplus/zerochplus/trunk@151 567da1b6-7cfc-4bc0-8471-d03287871468

--- test/module/varda.pl
+++ test/module/varda.pl
@@ -572,7 +572,7 @@ sub PrintIndexFoot
 	$cgipath	= $SYS->Get('CGIPATH');
 	$bbs		= $SYS->Get('BBS');
 	$ver		= $SYS->Get('VERSION');
-	$samba		= $this->{'SET'}->Get('BBS_SAMBATIME');
+	$samba		= $this->{'SET'}->Get('BBS_SAMBATIME')||$SYS->Get('SAMBATM');
 	$tm			= time;
 	
 	# スレッド作成画面を別画面で表示
