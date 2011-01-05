7a579285e0504c59def17afeb869c4aee8d85f30
iroiro123 <uekawa123+github_iroiro123@gmail.com>
2011-01-05 05:36:34 +0000

    zerochplus trunk/0.5.x ・携帯版微修正
    git-svn-id: http://svn.sourceforge.jp/svnroot/zerochplus/zerochplus/branches/0.5.x@181 567da1b6-7cfc-4bc0-8471-d03287871468

--- test/module/melkor.pl
+++ test/module/melkor.pl
@@ -260,7 +260,7 @@ sub InitSystemValue
 		'DATA'		=> '/datas',								# 初期データ設置パス(*)
 		'BBSPATH'	=> '..',									# 掲示板設置パス(*)
 		'DEBUG'		=> 0,										# デバグモード(*)
-		'VERSION'	=> '0ch+ BBS 0.5.2 110104',					# CGIバージョン
+		'VERSION'	=> '0ch+ BBS 0.5.3 1101xx',					# CGIバージョン
 		'PM-DAT'	=> 0644,									# datパーミション(*)
 		'PM-TXT'	=> 0644,									# TXTパーミション(*)
 		'PM-LOG'	=> 0770,									# LOGパーミション(*)
