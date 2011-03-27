9bd771c760d9833f4836db55601edde963fbe0ce
iroiro123 <uekawa123+github_iroiro123@gmail.com>
2011-03-27 02:23:49 +0000

    zerochplus 0.5.5 ・r240 released.
    zerochplus dev-r240
    ・r240 released.
    
    git-svn-id: http://svn.sourceforge.jp/svnroot/zerochplus/zerochplus/tags/dev-r240@244 567da1b6-7cfc-4bc0-8471-d03287871468

---
+++ test/module/constant.pl
@@ -0,0 +1,53 @@
+#============================================================================================================
+#
+#	定数モジュール(ZP)
+#	constant.pl
+#
+#	by ぜろちゃんねるプラス
+#	http://zerochplus.sourceforge.jp/
+#
+#	---------------------------------------------
+#
+#	2011.01.02 start
+#
+#============================================================================================================
+package	ZP;
+
+use strict;
+use warnings;
+use bigint;
+
+# CLIENT
+#  M: Mobile Browser, F: Full Browser
+our $C_PC				= 0x00000001;
+our $C_P2				= 0x00000002;
+our $C_DOCOMO_M			= 0x00000004;
+our $C_DOCOMO_F			= 0x00000008;
+our $C_DOCOMO			= $C_DOCOMO_M | $C_DOCOMO_F;
+our $C_AU_M				= 0x00000010;
+our $C_AU_F				= 0x00000020;
+our $C_AU				= $C_AU_M | $C_AU_F;
+our $C_SOFTBANK_M		= 0x00000040;
+our $C_SOFTBANK_F		= 0x00000080;
+our $C_SOFTBANK			= $C_SOFTBANK_M | $C_SOFTBANK_F;
+our $C_WILLCOM_M		= 0x00000100;
+our $C_WILLCOM_F		= 0x00000200;
+our $C_WILLCOM			= $C_WILLCOM_M | $C_WILLCOM_F;
+our $C_EMOBILE_M		= 0x00000400;
+our $C_EMOBILE_F		= 0x00000800;
+our $C_EMOBILE			= $C_EMOBILE_M | $C_EMOBILE_F;
+our $C_IBIS				= 0x00001000;
+our $C_JIG				= 0x00002000;
+our $C_OPERAMINI		= 0x00004000;
+our $C_IPHONE_F			= 0x00008000;
+our $C_IPHONEWIFI		= 0x00010000;
+our $C_IPHONE			= $C_IPHONE_F | $C_IPHONEWIFI;
+our $C_FBSERVICE		= $C_IBIS | $C_JIG | $C_OPERAMINI;
+our $C_MOBILEBROWSER	= $C_DOCOMO_M | $C_AU_M | $C_SOFTBANK_M | $C_WILLCOM_M | $C_EMOBILE_M;
+our $C_FULLBROWSER		= $C_DOCOMO_F | $C_AU_F | $C_SOFTBANK_F | $C_WILLCOM_F | $C_EMOBILE_F | $C_IPHONE | $C_FBSERVICE;
+our $C_MOBILE			= $C_MOBILEBROWSER | $C_FULLBROWSER;
+
+#============================================================================================================
+#	モジュール終端
+#============================================================================================================
+1;
