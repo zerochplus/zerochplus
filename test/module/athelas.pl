8f67875544657187fa596434050cc3b9d753540e
iroiro123 <uekawa123+github_iroiro123@gmail.com>
2011-07-30 11:33:26 +0000

    zerochplus 0.5.7/dev-r259 ・tags整理
    git-svn-id: http://svn.sourceforge.jp/svnroot/zerochplus/zerochplus/tags/dev-r259@260 567da1b6-7cfc-4bc0-8471-d03287871468

---
+++ test/module/athelas.pl
@@ -0,0 +1,601 @@
+#============================================================================================================
+#
+#	プラグイン管理モジュール
+#	athelas.pl
+#	-------------------------------------------------------------------------------------
+#	2005.02.19 start
+#
+#============================================================================================================
+package	ATHELAS;
+
+use strict;
+use warnings;
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	コンストラクタ
+#	-------------------------------------------------------------------------------------
+#	@param	なし
+#	@return	モジュールオブジェクト
+#
+#------------------------------------------------------------------------------------------------------------
+sub new
+{
+	my $this = shift;
+	my ($obj, %FILES, %CLASSES, %NAMES, %EXPS, %TYPES, %VALIDS, %CONFIGS, %CONFTYPES, @ORDER);
+	
+	$obj = {
+		'FILE'		=> \%FILES,
+		'CLASS'		=> \%CLASSES,
+		'NAME'		=> \%NAMES,
+		'EXPL'		=> \%EXPS,
+		'TYPE'		=> \%TYPES,
+		'VALID'		=> \%VALIDS,
+		'CONFIG'	=> \%CONFIGS,
+		'CONFTYPE'	=> \%CONFTYPES,
+		'ORDER'		=> \@ORDER,
+	};
+	bless $obj, $this;
+	return $obj;
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン情報読み込み
+#	-------------------------------------------------------------------------------------
+#	@param	$Sys	MELKOR
+#	@return	なし
+#
+#	2010.08.16 色々
+#	-> プラグインの個別設定
+#
+#------------------------------------------------------------------------------------------------------------
+sub Load
+{
+	my $this = shift;
+	my ($Sys) = @_;
+	my ($path, @elem);
+	
+	# ハッシュ初期化
+	undef $this->{'FILE'};
+	undef $this->{'CLASS'};
+	undef $this->{'NAME'};
+	undef $this->{'EXPL'};
+	undef $this->{'TYPE'};
+	undef $this->{'VALID'};
+	undef $this->{'CONFIG'};
+	undef $this->{'CONFTYPE'};
+	undef $this->{'ORDER'};
+	
+	$path = '.' . $Sys->Get('INFO') . '/plugins.cgi';
+	
+	if	(-e $path) {
+		open PLUGINS, "< $path";
+		my $i = 0;
+		while (<PLUGINS>) {
+			chomp $_;
+			@elem = split(/<>/, $_);
+			if (@elem >= 6) {
+				$this->{'FILE'}->{$elem[0]}		= $elem[1];
+				$this->{'CLASS'}->{$elem[0]}	= $elem[2];
+				$this->{'NAME'}->{$elem[0]}		= $elem[3];
+				$this->{'EXPL'}->{$elem[0]}		= $elem[4];
+				$this->{'TYPE'}->{$elem[0]}		= $elem[5];
+				$this->{'VALID'}->{$elem[0]}	= $elem[6];
+				$this->{'CONFIG'}->{$elem[0]}	= {};
+				$this->{'CONFTYPE'}->{$elem[0]}	= {};
+				$this->{'ORDER'}->[$i++]		= $elem[0];
+				$this->LoadConfig($elem[0]);
+			}
+		}
+		close PLUGINS;
+	}
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン個別設定読み込み (0ch+ Only)
+#	-------------------------------------------------------------------------------------
+#	@param	$id	
+#	@return	なし
+#
+#	2010.08.16 色々
+#	-> プラグインの個別設定
+#
+#------------------------------------------------------------------------------------------------------------
+sub LoadConfig
+{
+	my $this = shift;
+	my ($id) = @_;
+	my ($file, $path, $CONFIG, $CONFTYPE);
+	
+	$file = $this->{'FILE'}->{$id};
+	$CONFIG = $this->{'CONFIG'}->{$id};
+	$CONFTYPE = $this->{'CONFTYPE'}->{$id};
+	
+	$file =~ /^(0ch_.*)\.pl$/;
+	$path = "./plugin_conf/$1.cgi";
+	if (-f $path) {
+		open CONF, $path;
+		while (<CONF>) {
+			chomp $_;
+			my ($type, $key, $val) = split(/<>/, $_, 3);
+			$CONFIG->{$key} = $val;
+			$CONFTYPE->{$key} = $type;
+		}
+	}
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン個別設定保存 (0ch+ Only)
+#	-------------------------------------------------------------------------------------
+#	@param	$id	
+#	@return	なし
+#
+#	2010.08.16 色々
+#	-> プラグインの個別設定
+#
+#------------------------------------------------------------------------------------------------------------
+sub SaveConfig
+{
+	my $this = shift;
+	my ($id) = @_;
+	my ($file, $path, $CONFIG, $CONFTYPE);
+	
+	$file = $this->{'FILE'}->{$id};
+	$CONFIG = $this->{'CONFIG'}->{$id};
+	$CONFTYPE = $this->{'CONFTYPE'}->{$id};
+	
+	mkdir './plugin_conf' if (! -e './plugin_conf');
+	
+	$file =~ /^(0ch_.*)\.pl$/;
+	$path = "./plugin_conf/$1.cgi";
+	if (($_ = keys %$CONFIG) > 0) {
+		if (open CONF, "> $path") {
+			my ($key, $val, $type);
+			foreach $key (sort keys %$CONFIG) {
+				next unless (defined $CONFIG->{$key});
+				$val = $CONFIG->{$key};
+				$type = $CONFTYPE->{$key};
+				if ($type == 1) {
+					$val -= 0;
+				}
+				elsif ($type == 2) {
+					$val =~ s/\r\n|[\r\n]/<br>/g;
+					$val =~ s/<>/&lt;&gt;/g;
+				}
+				elsif ($type == 3) {
+					$val = ($val ? 1 : 0);
+				}
+				print CONF "$type<>$key<>$val\n";
+			}
+			close CONF;
+		}
+	}
+	else {
+		unlink $path;
+	}
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン個別設定初期値設定 (0ch+ Only)
+#	-------------------------------------------------------------------------------------
+#	@param	$id	
+#	@return	なし
+#
+#	2010.08.16 色々
+#	-> プラグインの個別設定
+#
+#------------------------------------------------------------------------------------------------------------
+sub SetDefaultConfig
+{
+	my $this = shift;
+	my ($id) = @_;
+	my ($file, $path, $CONFIG, $CONFTYPE, $className);
+	
+	$file = $this->{'FILE'}->{$id};
+	$CONFIG = $this->{'CONFIG'}->{$id};
+	$CONFTYPE = $this->{'CONFTYPE'}->{$id};
+	
+	%$CONFIG = ();
+	%$CONFTYPE = ();
+	
+	$file =~ /^0ch_(.*)\.pl$/;
+	$className = "ZPL_$1";
+	
+	require "./plugin/$file";
+	my $plugin = new $className;
+	if ($className->can('getConfig')) {
+		my $conf = $plugin->getConfig();
+		foreach my $key (keys %$conf) {
+			$CONFIG->{$key} = $conf->{$key}->{'default'};
+			$CONFTYPE->{$key} = $conf->{$key}->{'valuetype'};
+		}
+	}
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン情報保存
+#	-------------------------------------------------------------------------------------
+#	@param	$Sys	MELKOR
+#	@return	なし
+#
+#------------------------------------------------------------------------------------------------------------
+sub Save
+{
+	my $this = shift;
+	my ($Sys) = @_;
+	my ($path, $data, $id);
+	
+	$path = '.' . $Sys->Get('INFO') . '/plugins.cgi';
+	
+#	eval
+	{
+		open PLUGINS, "> $path";
+		flock PLUGINS, 2;
+		binmode PLUGINS;
+		#truncate PLUGINS, 0;
+		#seek PLUGINS, 0, 0;
+		foreach my $id (@{$this->{'ORDER'}}) {
+			$data = join('<>',
+				$id,
+				$this->{'FILE'}->{$id},
+				$this->{'CLASS'}->{$id},
+				$this->{'NAME'}->{$id},
+				$this->{'EXPL'}->{$id},
+				$this->{'TYPE'}->{$id},
+				$this->{'VALID'}->{$id}
+			);
+			
+			print PLUGINS "$data\n";
+		}
+		close PLUGINS;
+		chmod $Sys->Get('PM-ADM'), $path;
+	};
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグインIDセット取得
+#	-------------------------------------------------------------------------------------
+#	@param	$kind	検索種別
+#	@param	$name	検索ワード
+#	@param	$pBuf	IDセット格納バッファ
+#	@return	キーセット数
+#
+#------------------------------------------------------------------------------------------------------------
+sub GetKeySet
+{
+	my $this = shift;
+	my ($kind, $name, $pBuf) = @_;
+	my ($key, $n);
+	
+	$n = 0;
+	
+	if ($kind eq 'ALL') {
+		foreach $key (@{$this->{'ORDER'}}) {
+			push @$pBuf, $key;
+			$n++;
+		}
+	}
+	else {
+		foreach $key (@{$this->{'ORDER'}}) {
+			if ($this->{$kind}->{$key} eq $name || $name eq 'ALL') {
+				push @$pBuf, $key;
+				$n++;
+			}
+		}
+	}
+	
+	return $n;
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン情報取得
+#	-------------------------------------------------------------------------------------
+#	@param	$kind	情報種別
+#	@param	$key	ユーザID
+#			$default : デフォルト
+#	@return	ユーザ情報
+#
+#------------------------------------------------------------------------------------------------------------
+sub Get
+{
+	my $this = shift;
+	my ($kind, $key, $default) = @_;
+	my ($val);
+	
+	$val = $this->{$kind}->{$key};
+	
+	return (defined $val ? $val : (defined $default ? $default : undef));
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン情報設定
+#	-------------------------------------------------------------------------------------
+#	@param	$id		ユーザID
+#	@param	$kind	情報種別
+#	@param	$val	設定値
+#	@return	なし
+#
+#------------------------------------------------------------------------------------------------------------
+sub Set
+{
+	my $this = shift;
+	my ($id, $kind, $val) = @_;
+	
+	if (exists $this->{$kind}->{$id}) {
+		$this->{$kind}->{$id} = $val;
+	}
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン追加
+#	-------------------------------------------------------------------------------------
+#	@param	$file	プラグインファイル名
+#	@param	$valid	有効フラグ
+#	@return	プラグインID
+#
+#------------------------------------------------------------------------------------------------------------
+sub Add
+{
+	my $this = shift;
+	my ($file, $valid) = @_;
+	my ($id, $ret);
+	
+	$ret = undef;
+	$id = time;
+	while (exists $this->{'FILE'}->{$id}) {
+		$id++;
+	}
+	if (-e "./plugin/$file") {
+		if ($file =~ /0ch_(.*)\.pl/) {
+			my $className = "ZPL_$1";
+#			eval
+			{
+				require "./plugin/$file";
+				my $plugin = new $className;
+				if (! exists $this->{'FILE'}->{$id}) {
+					push @{$this->{'ORDER'}}, $id;
+				}
+				$this->{'FILE'}->{$id}		= $file;
+				$this->{'CLASS'}->{$id}		= $className;
+				$this->{'NAME'}->{$id}		= $plugin->getName();
+				$this->{'EXPL'}->{$id}		= $plugin->getExplanation();
+				$this->{'TYPE'}->{$id}		= $plugin->getType();
+				$this->{'VALID'}->{$id}		= $valid;
+				$this->{'CONFIG'}->{$id}	= {};
+				$this->{'CONFTYPE'}->{$id}	= {};
+				$this->SetDefaultConfig($id);
+				$this->LoadConfig($id);
+				$this->SaveConfig($id);
+				$ret = $id;
+			};
+		}
+	}
+	return '';
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン情報削除
+#	-------------------------------------------------------------------------------------
+#	@param	$id		削除プラグインID
+#	@return	なし
+#
+#------------------------------------------------------------------------------------------------------------
+sub Delete
+{
+	my $this = shift;
+	my ($id) = @_;
+	
+	delete $this->{'FILE'}->{$id};
+	delete $this->{'CLASS'}->{$id};
+	delete $this->{'NAME'}->{$id};
+	delete $this->{'EXPL'}->{$id};
+	delete $this->{'TYPE'}->{$id};
+	delete $this->{'VALID'}->{$id};
+	delete $this->{'CONFIG'}->{$id};
+	delete $this->{'CONFTYPE'}->{$id};
+	
+	$_ = $this->{'ORDER'};
+	for (my $i = 0 ; $i < $#{$_} ; $i++) {
+		splice(@$_, $i--, 1) if ($_->[$i] eq $id);
+	}
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン情報更新
+#	-------------------------------------------------------------------------------------
+#	@param	なし
+#	@return	なし
+#
+#------------------------------------------------------------------------------------------------------------
+sub Update
+{
+	my $this = shift;
+	my (@files, $file, $plugin, @buff, $exist);
+	
+	if (-e './plugin') {
+		opendir PLUGINS, './plugin';
+		@files = readdir PLUGINS;
+		closedir PLUGINS;
+		# プラグイン追加・更新フェイズ
+		foreach $file (@files) {
+			if ($file =~ /^0ch_(.*)\.pl/) {
+				my $className = "ZPL_$1";
+				if ($this->GetKeySet('FILE', $file, \@buff) > 0) {
+					require "./plugin/$file";
+					$plugin = new $className;
+					$this->{'NAME'}->{$buff[0]} = $plugin->getName();
+					$this->{'EXPL'}->{$buff[0]} = $plugin->getExplanation();
+					$this->{'TYPE'}->{$buff[0]} = $plugin->getType();
+					$this->SetDefaultConfig($buff[0]);
+					$this->LoadConfig($buff[0]);
+					$this->SaveConfig($buff[0]);
+					$plugin = undef;
+				}
+				else {
+					$this->Add($file, 0);
+				}
+				undef @buff;
+			}
+		}
+		# プラグイン削除フェイズ
+		if ($this->GetKeySet('ALL', '', \@buff) > 0) {
+			$exist = 0;
+			foreach $plugin (@buff) {
+				foreach $file (@files) {
+					if ($this->Get('FILE', $plugin) eq $file) {
+						$exist = 1;
+						last;
+					}
+				}
+				if ($exist == 0) {
+					$this->Delete($plugin);
+				}
+				$exist = 0;
+			}
+		}
+	}
+	else {
+		undef $this->{'FILE'};
+		undef $this->{'CLASS'};
+		undef $this->{'NAME'};
+		undef $this->{'EXPL'};
+		undef $this->{'TYPE'};
+		undef $this->{'VALID'};
+		undef $this->{'CONFIG'};
+		undef $this->{'CONFTYPE'};
+		undef $this->{'ORDER'};
+	}
+}
+
+
+#============================================================================================================
+#
+#	プラグイン個別設定管理モジュール (0ch+ Only)
+#
+#	-------------------------------------------------------------------------------------
+#	2010.08.20 start 色々
+#
+#============================================================================================================
+
+package	PLUGINCONF;
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	コンストラクタ
+#	-------------------------------------------------------------------------------------
+#	@param	$Plugin	ATHELAS
+#	@param	$id		
+#	@return	モジュールオブジェクト
+#
+#------------------------------------------------------------------------------------------------------------
+sub new
+{
+	my $this = shift;
+	my ($Plugin, $id) = @_;
+	my ($obj);
+	
+	$obj = {
+		'PLUGIN'	=> $Plugin,
+		'id'		=> $id
+	};
+	
+	bless $obj, $this;
+	return $obj;
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン個別設定設定 (0ch+ Only)
+#	-------------------------------------------------------------------------------------
+#	@param	$key	
+#	@param	$val	
+#	@return	なし
+#
+#	2010.08.16 色々
+#	-> プラグインの個別設定
+#
+#------------------------------------------------------------------------------------------------------------
+sub SetConfig
+{
+	my $this = shift;
+	my ($key, $val) = @_;
+	my ($CONFIG, $CONFTYPE, $id, $type);
+	
+	$id = $this->{'id'};
+	$CONFIG = $this->{'PLUGIN'}->{'CONFIG'}->{$id};
+	$CONFTYPE = $this->{'PLUGIN'}->{'CONFTYPE'}->{$id};
+	
+	if (! defined $CONFTYPE->{$key}) {
+		if (ref(\$val) eq 'SCALAR') {
+	#		if (($val ^ $val) eq '0') {
+	#			$type = 1;
+	#		}
+	#		else {
+	#			$type = 2;
+	#		}
+			$type = 2;
+		}
+		else {
+			$type = 0;
+			return;
+		}
+		$CONFTYPE->{$key} = $type;
+	}
+	else {
+		$type = $CONFTYPE->{$key};
+	}
+	
+	if ($type == 1) {
+		$val -= 0;
+	}
+	elsif ($type == 2) {
+		$val =~ s/\r\n|[\r\n]/<br>/g;
+		$val =~ s/<>/&lt;&gt;/g;
+	}
+	elsif ($type == 3) {
+		$val = ($val ? 1 : 0);
+	}
+	
+	$CONFIG->{$key} = $val;
+	
+	$this->{'PLUGIN'}->SaveConfig($id);
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	プラグイン個別設定取得 (0ch+ Only)
+#	-------------------------------------------------------------------------------------
+#	@param	$key	
+#	@return	プラグイン個別設定
+#
+#	2010.08.16 色々
+#	-> プラグインの個別設定
+#
+#------------------------------------------------------------------------------------------------------------
+sub GetConfig
+{
+	my $this = shift;
+	my ($key) = @_;
+	my ($CONFIG, $id);
+	
+	$id = $this->{'id'};
+	$CONFIG = $this->{'PLUGIN'}->{'CONFIG'}->{$id};
+	
+	return $CONFIG->{$key};
+}
+
+#============================================================================================================
+#	Module END
+#============================================================================================================
+1;
---
+++ test/module/isildur.pl
@@ -0,0 +1,345 @@
+#============================================================================================================
+#
+#	SETTINGデータ管理モジュール(ISILDUR)
+#	isuldur.pl
+#	--------------------------------------------
+#	2002.12.03 start
+#	2003.02.15 データ構造の変更
+#
+#============================================================================================================
+package	ISILDUR;
+
+use strict;
+use warnings;
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	コンストラクタ
+#	-------------------------------------------------------------------------------------
+#	@param	なし
+#	@return	モジュールオブジェクト
+#
+#------------------------------------------------------------------------------------------------------------
+sub new
+{
+	my $this = shift;
+	my (%SET, $obj);
+	
+	$obj = {
+		'SETTING'	=> \%SET
+	};
+	bless $obj, $this;
+	
+	return $obj;
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	掲示板設定読み込み
+#	-------------------------------------------------------------------------------------
+#	@param	なし
+#	@return	なし
+#
+#------------------------------------------------------------------------------------------------------------
+sub Load
+{
+	my $this = shift;
+	my ($Sys) = @_;
+	my ($path, $key, $val);
+	
+	undef %{$this->{'SETTING'}};
+	InitSettingData($this->{'SETTING'});
+	
+	$path = $Sys->Get('BBSPATH') . '/' . $Sys->Get('BBS') . '/SETTING.TXT';
+	
+	if (-e $path) {
+#		eval
+		{
+			open SETTING, "< $path";
+			while (<SETTING>) {
+				chomp $_;
+				($key, $val) = split(/=/, $_);
+				$this->{'SETTING'}->{$key} = $val;
+			}
+			close SETTING;
+		};
+		return 1;
+	}
+	return 0;
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	掲示板設定書き込み
+#	-------------------------------------------------------------------------------------
+#	@param	なし
+#	@return	なし
+#
+#------------------------------------------------------------------------------------------------------------
+sub Save
+{
+	my $this = shift;
+	my ($Sys) = @_;
+	my ($path, $key, $val, @ch2setting, %orz);
+	
+	$path = $Sys->Get('BBSPATH') . '/' . $Sys->Get('BBS') . '/SETTING.TXT';
+	
+	# ２ちゃんねるのSETTING.TXT順序
+	@ch2setting = (
+		'BBS_TITLE', 'BBS_TITLE_PICTURE', 'BBS_TITLE_COLOR', 'BBS_TITLE_LINK', 'BBS_BG_COLOR',
+		'BBS_BG_PICTURE', 'BBS_NONAME_NAME', 'BBS_MAKETHREAD_COLOR', 'BBS_MENU_COLOR', 'BBS_THREAD_COLOR',
+		'BBS_TEXT_COLOR', 'BBS_NAME_COLOR', 'BBS_LINK_COLOR', 'BBS_ALINK_COLOR', 'BBS_VLINK_COLOR',
+		'BBS_THREAD_NUMBER', 'BBS_CONTENTS_NUMBER', 'BBS_LINE_NUMBER', 'BBS_MAX_MENU_THREAD', 'BBS_SUBJECT_COLOR',
+		'BBS_PASSWORD_CHECK', 'BBS_UNICODE', 'BBS_DELETE_NAME', 'BBS_NAMECOOKIE_CHECK', 'BBS_MAILCOOKIE_CHECK',
+		'BBS_SUBJECT_COUNT', 'BBS_NAME_COUNT', 'BBS_MAIL_COUNT', 'BBS_MESSAGE_COUNT', 'BBS_NEWSUBJECT',
+		'BBS_THREAD_TATESUGI', 'BBS_AD2', 'SUBBBS_CGI_ON', 'NANASHI_CHECK', 'timecount', 'timeclose',
+		'BBS_PROXY_CHECK', 'BBS_OVERSEA_THREAD', 'BBS_OVERSEA_PROXY', 'BBS_RAWIP_CHECK', 'BBS_SLIP',
+		'BBS_DISP_IP', 'BBS_FORCE_ID', 'BBS_BE_ID', 'BBS_BE_TYPE2', 'BBS_NO_ID', 'BBS_JP_CHECK',
+		'BBS_VIP931', 'BBS_4WORLD', 'BBS_YMD_WEEKS', 'BBS_NINJA'
+	);
+	
+	%orz = %{$this->{'SETTING'}};
+	
+#	eval
+	{
+		open SETTING, "> $path";
+		flock SETTING, 2;
+		binmode SETTING;
+		#truncate SETTING, 0;
+		#seek SETTING, 0, 0;
+		# 順番に出力
+		foreach $key ( @ch2setting ) {
+			print SETTING "$key=" . $this->Get($key, '') . "\n";
+			delete $orz{$key};
+		}
+		foreach $key (sort keys %orz) {
+			#$val = $orz{$key};
+			print SETTING "$key=" . $this->Get($key, '') . "\n";
+		}
+=pod
+		foreach $key (sort keys %{$this->{'SETTING'}}) {
+			$val = $this->{'SETTING'}->{$key};
+			print SETTING "$key=$val\n";
+		}
+=cut
+		close SETTING;
+		chmod $Sys->Get('PM-TXT'), $path;
+	};
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	掲示板設定読み込み(指定ファイル)
+#	-------------------------------------------------------------------------------------
+#	@param	なし
+#	@return	なし
+#
+#------------------------------------------------------------------------------------------------------------
+sub LoadFrom
+{
+	my $this = shift;
+	my ($path) = @_;
+	my ($key, $val);
+	
+	undef %{$this->{'SETTING'}};
+	
+	if (-e $path) {
+#		eval
+		{
+			open SETTING, "< $path";
+			while (<SETTING>) {
+				chomp $_;
+				($key, $val) = split(/=/, $_);
+				$this->{'SETTING'}->{$key} = $val;
+			}
+			close SETTING;
+		};
+		return 1;
+	}
+	return 0;
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	掲示板設定書き込み(指定ファイル)
+#	-------------------------------------------------------------------------------------
+#	@param	なし
+#	@return	なし
+#
+#------------------------------------------------------------------------------------------------------------
+sub SaveAs
+{
+	my $this = shift;
+	my ($path) = @_;
+	my ($key, $val);
+	
+#	eval
+	{
+		open SETTING, "> $path";
+		flock SETTING, 2;
+		binmode SETTING;
+		#truncate SETTING, 0;
+		#seek SETTING, 0, 0;
+		foreach $key (keys %{$this->{'SETTING'}}) {
+			$val = $this->{'SETTING'}->{$key};
+			print SETTING "$key=$val\n";
+		}
+		close SETTING;
+		#chmod $Sys->Get('PM-TXT'), $path;
+	};
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	掲示板設定キー取得
+#	-------------------------------------------------------------------------------------
+#	@param	$keySet	キーセット格納バッファ
+#	@return	なし
+#
+#------------------------------------------------------------------------------------------------------------
+sub GetKeySet
+{
+	my $this = shift;
+	my ($keySet) = @_;
+	
+	foreach (keys %{$this->{'SETTING'}}) {
+		push @$keySet, $_;
+	}
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	掲示板設定値比較
+#	-------------------------------------------------------------------------------------
+#	@param	$key	設定キー
+#	@param	$val	設定値
+#	@return	同等なら真を返す
+#
+#------------------------------------------------------------------------------------------------------------
+sub Equal
+{
+	my $this = shift;
+	my ($key, $val) = @_;
+	
+	return(defined $this->{'SETTING'}->{$key} && $this->{'SETTING'}->{$key} eq $val);
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	掲示板設定値取得
+#	-------------------------------------------------------------------------------------
+#	@param	$key	設定キー
+#			$default : デフォルト
+#	@return	設定値
+#
+#------------------------------------------------------------------------------------------------------------
+sub Get
+{
+	my $this = shift;
+	my ($key, $default) = @_;
+	my ($val);
+	
+	$val = $this->{'SETTING'}->{$key};
+	
+	return (defined $val ? $val : (defined $default ? $default : undef));
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	掲示板設定値設定
+#	-------------------------------------------------------------------------------------
+#	@param	$key	設定キー
+#	@param	$val	設定値
+#	@return	なし
+#
+#------------------------------------------------------------------------------------------------------------
+sub Set
+{
+	my $this = shift;
+	my ($key, $val) = @_;
+	
+	$this->{'SETTING'}->{$key} = $val;
+}
+
+#------------------------------------------------------------------------------------------------------------
+#
+#	SETTING項目初期化 - InitSettingData
+#	-------------------------------------------
+#	引　数：$pSET : ハッシュの参照
+#	戻り値：なし
+#
+#------------------------------------------------------------------------------------------------------------
+sub InitSettingData
+{
+	my ($pSET) = @_;
+	
+	%$pSET = (
+		# ２ちゃんねる互換設定項目
+		'BBS_TITLE'				=> '掲示板＠ぜろちゃんねるプラス',
+		'BBS_TITLE_PICTURE'		=> 'kanban.gif',
+		'BBS_TITLE_COLOR'		=> '#000000',
+		'BBS_TITLE_LINK'		=> 'http://zerochplus.sourceforge.jp/',
+		'BBS_BG_COLOR'			=> '#FFFFFF',
+		'BBS_BG_PICTURE'		=> 'ba.gif',
+		'BBS_NONAME_NAME'		=> '名無しさん＠ぜろちゃんねるプラス',
+		'BBS_MAKETHREAD_COLOR'	=> '#CCFFCC',
+		'BBS_MENU_COLOR'		=> '#CCFFCC',
+		'BBS_THREAD_COLOR'		=> '#EFEFEF',
+		'BBS_TEXT_COLOR'		=> '#000000',
+		'BBS_NAME_COLOR'		=> 'green',
+		'BBS_LINK_COLOR'		=> '#0000FF',
+		'BBS_ALINK_COLOR'		=> '#FF0000',
+		'BBS_VLINK_COLOR'		=> '#AA0088',
+		'BBS_THREAD_NUMBER'		=> 10,
+		'BBS_CONTENTS_NUMBER'	=> 10,
+		'BBS_LINE_NUMBER'		=> 12,
+		'BBS_MAX_MENU_THREAD'	=> 30,
+		'BBS_SUBJECT_COLOR'		=> '#FF0000',
+		'BBS_PASSWORD_CHECK'	=> 'checked',
+		'BBS_UNICODE'			=> 'pass',
+		'BBS_DELETE_NAME'		=> 'あぼーん',
+		'BBS_NAMECOOKIE_CHECK'	=> 'checked',
+		'BBS_MAILCOOKIE_CHECK'	=> 'checked',
+		'BBS_SUBJECT_COUNT'		=> 48,
+		'BBS_NAME_COUNT'		=> 128,
+		'BBS_MAIL_COUNT'		=> 64,
+		'BBS_MESSAGE_COUNT'		=> 2048,
+		'BBS_NEWSUBJECT'		=> 1,
+		'BBS_THREAD_TATESUGI'	=> 5,
+		'BBS_AD2'				=> '',
+		'SUBBBS_CGI_ON'			=> 1,
+		'NANASHI_CHECK'			=> '',
+		'timecount'				=> 7,
+		'timeclose'				=> 5,
+		'BBS_PROXY_CHECK'		=> '',
+		'BBS_OVERSEA_THREAD'	=> '',
+		'BBS_OVERSEA_PROXY'		=> '',
+		'BBS_RAWIP_CHECK'		=> '',
+		'BBS_SLIP'				=> '',
+		'BBS_DISP_IP'			=> 'checked',
+		'BBS_FORCE_ID'			=> '',
+		'BBS_BE_ID'				=> '',
+		'BBS_BE_TYPE2'			=> '',
+		'BBS_NO_ID'				=> '',
+		'BBS_JP_CHECK'			=> '',
+		'BBS_YMD_WEEKS'			=> '日/月/火/水/木/金/土',
+		'BBS_NINJA'				=> '',
+		
+		# 以下0chオリジナル設定項目
+		'BBS_DATMAX'			=> 512,
+		'BBS_COOKIEPATH'		=> '/',
+		'BBS_READONLY'			=> 'caps',
+		'BBS_REFERER_CUSHION'	=> 'ime.nu/',
+		'BBS_THREADCAPONLY'		=> '',
+		'BBS_TRIPCOLUMN'		=> 10,
+		'BBS_SUBTITLE'			=> 'またーり雑談',
+		'BBS_COLUMN_NUMBER'		=> 256,
+		'BBS_SAMBATIME'			=> '',
+		'BBS_HOUSHITIME'		=> '',
+	);
+}
+
+#============================================================================================================
+#	Module END
+#============================================================================================================
+1;
